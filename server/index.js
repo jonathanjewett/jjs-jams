6010a51705ff2083cbb94438a89b01374dabcb9a
jonathanjewett <jonathanjewett0@gmail.com>
2021-12-04 05:41:27 -0600

    get request to /artists works, next will work on setting up database

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/artists', (req, res) => {
+    res.status(200).send('get for /artists working');
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
