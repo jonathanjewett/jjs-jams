18bdd01adc4b5909581b1e9f6418c107bf60e8d5
jonathanjewett <jonathanjewett0@gmail.com>
2021-12-03 18:43:40 -0600

    changed background color to black and added basic table

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Results from './components/Results.jsx';
+
+function App() {
+  return (
+    <div>
+      <h1>New Beginnings</h1>
+      <Results />
+    </div>
+  );
+}
+
+export default App;
